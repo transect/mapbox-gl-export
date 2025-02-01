13c2e18cfb822de2417f829e3bb85bae2ccf2708
Umar Muneer <umar.muneer@conradlabs.com>
2025-02-01 08:57:06 +0500

    chore: add map generator class to export

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
-export { default as MapboxExportControl} from './export-control';
-export { Size, PageOrientation, Format, DPI} from './map-generator';
+export { default as MapboxExportControl } from "./export-control";
+export { Size, PageOrientation, Format, DPI } from "./map-generator";
+export { default as MapGenerator } from "./map-generator";
